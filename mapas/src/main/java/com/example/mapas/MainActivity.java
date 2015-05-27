cd1bc755187b8d502d510ddd62a58b93d0e0f88a
Edwin <edwin.cubillos@udea.edu.co>
2015-05-27 11:36:24 -0500

    Versión2.0 Captura posicion actual del usuario de la aplicacion

--- mapas/src/main/java/com/example/mapas/MainActivity.java
+++ mapas/src/main/java/com/example/mapas/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.mapas;
 
-import android.graphics.Camera;
+import android.graphics.Bitmap;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap map;
     private CameraUpdate cameraUpdate;
@@ -26,51 +34,125 @@ public class MainActivity extends FragmentActivity {
     private final LatLng LOCATION_HOME = new LatLng(6.176627f, -75.612701f);
     private final LatLng LOCATION_UDEA = new LatLng(6.267577f, -75.568995f);
 
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private LatLng currentLocation;
+
+    private boolean newLocationReady = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        map =((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
         map.addMarker(new MarkerOptions().position(LOCATION_HOME).title("My Home").snippet("Carrera 54 N 77-41")
-        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
         map.addMarker(new MarkerOptions().position(LOCATION_UDEA).title("University of Antioquia").snippet("Calle 67 #53-108"));
+
+        buildGoogleApiClient();
+        createLocationRequest();
+
     }
 
-    public void onClickHome(View view){
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    public void onClickHome(View view) {
         map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        cameraUpdate= CameraUpdateFactory.newLatLngZoom(LOCATION_HOME,16);
+        cameraUpdate = CameraUpdateFactory.newLatLngZoom(LOCATION_HOME, 16);
         map.animateCamera(cameraUpdate);
     }
 
-    public void onClickU(View view){
+    public void onClickU(View view) {
         map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        cameraUpdate = CameraUpdateFactory.newLatLngZoom(LOCATION_UDEA,16);
+        cameraUpdate = CameraUpdateFactory.newLatLngZoom(LOCATION_UDEA, 16);
         map.animateCamera(cameraUpdate);
+    }
+
+    public void onClickCurrent(View view){
+        if (newLocationReady){
+            map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLocation,16);
+            map.animateCamera(cameraUpdate);
+        }
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onConnected(Bundle bundle) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null){
+            setNewLocation(mLastLocation);
+            newLocationReady = true;
+        }
+        else {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest, this);
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        setNewLocation(location);
+        newLocationReady = true;
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    public void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void setNewLocation(Location location){
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        currentLocation = new LatLng(latitude, longitude);
+        map.addMarker(new MarkerOptions().position(currentLocation).title("Find me here")
+        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+    }
+
+
+
 }
+
+
+
